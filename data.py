d8a8b42050a66089c39099eb1bc1e0ceea6f1921
Idolphint <litiantianper@buaa.com>
2022-06-26 16:07:56 +0800

    svm方法roc到了0.75

--- data.py
+++ data.py
@@ -4,10 +4,12 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import torch
 
+code_regi_redict = {}
+hy_dm_redict = {}
+
 
 class Company(object):
     def __init__(self, data):
-
         id, open_date, ratal_class, code_regi, hy_dm, hyzl_dm, hydl_dm, hyml_dm = data
         open_day = open_date.split(' ')[0]
         open_day = open_day.split('-')
@@ -16,10 +18,13 @@ class Company(object):
         # print(ratal_class)
         self.id = id
         self.enterprise_opening_date = open_date
-        # self.code_enterprise_ratal_classes = ratal_class
-        # self.code_enterprise_registration = code_regi
-        self.HY_DM = [int(ratal_class), code_regi, int(hy_dm[-1]),
-                      int(hy_dm[-2]), int(hy_dm[:-2]), ord(hyml_dm)-ord('A')]  # 行业代码、中类、大类、门类
+        if code_regi not in code_regi_redict.keys():
+            code_regi_redict[code_regi] = len(code_regi_redict.keys())
+        if hy_dm[:-2] not in hy_dm_redict.keys():
+            hy_dm_redict[hy_dm[:-2]] = len(hy_dm_redict.keys())
+        self.HY_DM = [int(ratal_class), code_regi_redict[code_regi], int(hy_dm[-1]),
+                      int(hy_dm[-2]), hy_dm_redict[hy_dm[:-2]], ord(hyml_dm) - ord('A')]  # 行业代码、中类、大类、门类
+
         self.tax_return = {}
         self.tax_pay = {}
         self.investor_info = []
@@ -41,25 +46,46 @@ class Company(object):
 
     # 数据归一化
     def get_four_investor(self):
-        sort_inv = sorted(self.investor_info, key=lambda x: x[2])
-        out_inv = np.zeros((4,2))
+        sort_inv = sorted(self.investor_info, key=lambda x: x[2], reverse=True)
+        out_inv = np.zeros((4, 2))
         for i in range(min(len(sort_inv), 4)):
-            out_inv[i,0] = sort_inv[i][2]
-            out_inv[i,1] = sort_inv[i][3] / 10000
+            out_inv[i, 0] = sort_inv[i][2]
+            out_inv[i, 1] = sort_inv[i][3]
         return out_inv
 
     def get_tax_return(self):
         key = sorted(self.tax_return.keys())
-        out_return = np.zeros((len(key), 2))
-        for i,k in enumerate(key):
+        out_return = []
+        item_dict = {}
+        for i, k in enumerate(key):
+            new_month = [0] * len(item_dict.keys())
             for item, info in self.tax_return[k].items():  # 类别应该分开来看吗？
-                # 补0还是填平均值
-                out_return[i, 0] += info[0] / 10000
-                out_return[i, 1] = info[1]
-        return out_return
+                if item not in item_dict.keys():
+                    item_dict[item] = [len(item_dict.keys()), 1]
+                    new_month.append(info[0])
+                else:
+                    new_month[item_dict[item][0]] = info[0]
+                    item_dict[item][1] += 1
+            out_return.append(new_month)
+            #     # 补0还是填平均值
+            #     out_return[i, 0] += info[0] / 10000
+            #     out_return[i, 1] = info[1]
+            # if out_return[i,0] < 0:
+            #     out_return[i,0] = -1*np.log(abs(out_return[i,0]))
+            # else:
+            #     out_return[i,0] = np.log(out_return[i,0]+1)
+        out_ret = np.zeros((len(key), len(item_dict.keys())))
+        item_sort = sorted(list(item_dict.values()), key=lambda x: x[1], reverse=True)
+        item_sort = np.array(item_sort)[:,0]
+        for i, m in enumerate(out_return):
+            m += [0] * (len(item_dict.keys()) - len(m))
+            for j in range(len(item_sort)):
+                out_ret[i,j] = m[item_sort[j]]
+
+        return out_ret
 
     def get_tax_pay(self):
-        out_pay = []
+        out_pay = {}
         for key, item in self.tax_pay.items():
             if item is None:
                 continue
@@ -68,17 +94,10 @@ class Company(object):
                     tax_re = self.tax_return[key][code_item][0]
                 else:
                     tax_re = 0
-                    cnt=0
-                    if key in self.tax_return:
-                        for code_item_r, re in self.tax_return[key].items():
-                            tax_re += re[0]
-                            cnt+=1
-                    if cnt > 0:
-                        tax_re /= cnt
-                out_pay.append([info[0]/10000, tax_re/10000, info[1], int(info[2])])
-        if len(out_pay) == 0:
-            out_pay.append([0,0,0,0])
-        return np.array(out_pay)
+                if code_item not in out_pay:
+                    out_pay[code_item] = []  # 交税，报税，税务类别
+                out_pay[code_item].append([info[0], tax_re, int(info[2])])
+        return out_pay
 
     def add_tax_return(self, data):
         id, tax_return_date, return_ddl, code_account, code_item, tax_begin, tax_end, sales = data
@@ -91,15 +110,15 @@ class Company(object):
         code_account = str(code_account)
         code_item = str(code_item)
 
-        assert code_account+code_item[len(code_account):] == code_item
+        assert code_account + code_item[len(code_account):] == code_item
         code_item = code_item[len(code_account):]
         sales = float(sales)
         # 相对于公司成立日期的月数
-        until_ddl = (return_ddl[1] - tax_return_date[1])*30 + return_ddl[2] - tax_return_date[2]
-        rela_tax_begin = (tax_begin[0]-self.enterprise_opening_date[0])*12 +\
-                         tax_begin[1]-self.enterprise_opening_date[1]
+        until_ddl = (return_ddl[1] - tax_return_date[1]) * 30 + return_ddl[2] - tax_return_date[2]
+        rela_tax_begin = (tax_begin[0] - self.enterprise_opening_date[0]) * 12 + \
+                         tax_begin[1] - self.enterprise_opening_date[1]
         if rela_tax_begin not in self.tax_return.keys():
-            self.tax_return[rela_tax_begin] = {code_item:[sales, until_ddl, code_account]}
+            self.tax_return[rela_tax_begin] = {code_item: [sales, until_ddl, code_account]}
         else:
             self.tax_return[rela_tax_begin][code_item] = [sales, until_ddl, code_account]
 
@@ -116,11 +135,11 @@ class Company(object):
         code_account = str(code_account)
         assert code_account + code_item[len(code_account):] == code_item
         code_item = code_item[len(code_account):]
-        until_ddl = (ddl[1]-pay_date[1])*30+(ddl[2]-pay_date[2])
-        rela_tax_begin = (pay_begin[0]-self.enterprise_opening_date[0])*12 +\
-                         pay_begin[1]-self.enterprise_opening_date[1]
+        until_ddl = (ddl[1] - pay_date[1]) * 30 + (ddl[2] - pay_date[2])
+        rela_tax_begin = (pay_begin[0] - self.enterprise_opening_date[0]) * 12 + \
+                         pay_begin[1] - self.enterprise_opening_date[1]
         if rela_tax_begin not in self.tax_pay.keys() or self.tax_pay[rela_tax_begin] is None:
-            self.tax_pay[rela_tax_begin] = {code_item:[ratal, until_ddl, tax_class, code_account]}
+            self.tax_pay[rela_tax_begin] = {code_item: [ratal, until_ddl, tax_class, code_account]}
         else:
             self.tax_pay[rela_tax_begin][code_item] = [ratal, until_ddl, tax_class, code_account]
 
@@ -144,13 +163,13 @@ class ComDataset(Dataset):
         return len(self.data)
 
 
-def init_dataset(mode = "train", eval_rate=0.9):
+def init_dataset(mode="train", eval_rate=0.9):
     dataset = {}
-    base_info = pd.read_csv("../data_jk/%s_basic_info.csv"%mode, encoding='utf-8')
-    tax_return = pd.read_csv("../data_jk/%s_tax_return_.csv"%mode, encoding='utf-8')
+    base_info = pd.read_csv("../data_jk/%s_basic_info.csv" % mode, encoding='utf-8')
+    tax_return = pd.read_csv("../data_jk/%s_tax_return_.csv" % mode, encoding='utf-8')
     tax_payment = pd.read_csv("../data_jk/%s_tax_payment_.csv" % mode, encoding='utf-8')
     tax_invest = pd.read_csv("../data_jk/%s_investor_info.csv" % mode, encoding='utf-8')
-    base_info.fillna({'code_enterprise_ratal_classes':'0', 'HYML_DM':'A'}, inplace=True)
+    base_info.fillna({'code_enterprise_ratal_classes': '0', 'HYML_DM': 'A'}, inplace=True)
     for index, row in base_info.iterrows():
         id = row['ID']
         dataset[id] = Company(row)
@@ -176,13 +195,13 @@ def init_dataset(mode = "train", eval_rate=0.9):
             comp = dataset[id]
             comp.add_label(row)
             dataset[id] = comp
-        eval_dataset = list(dataset.keys())[int(len(dataset.keys())*eval_rate):]
+        eval_dataset = list(dataset.keys())[int(len(dataset.keys()) * eval_rate):]
         eval_out = {ev: dataset[ev] for ev in eval_dataset}
         [dataset.pop(ev) for ev in eval_dataset]
         pickle.dump(dataset, open("../data_jk/train_data.pkl", 'wb'))
         pickle.dump(eval_out, open("../data_jk/eval_data.pkl", 'wb'))
     else:
-        pickle.dump(dataset, open("../data_jk/%s.pkl"%mode, 'wb'))
+        pickle.dump(dataset, open("../data_jk/%s.pkl" % mode, 'wb'))
 
     return dataset
 
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+import numpy as np
 import torch
 import matplotlib.pyplot as plt
 from sklearn import metrics
 from data import Company
 from functions import FocalLoss
+from sklearn.svm import SVC
 import pandas as pd
 import pickle
 from model import EmbeddingClassify, TaxPayModel, TaxReturnLSTM
 import torch.nn as nn
 import torch.nn.functional as F
 
-
+test = False
 train_f = open("../data_jk/train_data.pkl", 'rb')
 eval_f = open("../data_jk/eval_data.pkl", 'rb')
 test_f = open("../data_jk/test.pkl", 'rb')
@@ -30,12 +32,12 @@ optimizer1 = torch.optim.Adam(model1.parameters(), lr=lr, weight_decay=0.99)
 optimizer2 = torch.optim.Adam(model2.parameters(), lr=lr, weight_decay=0.99)
 optimizer3 = torch.optim.Adam(model3.parameters(), lr=lr, weight_decay=0.99)
 
-def draw_roc(save_path, gt, pred):
+def draw_roc(save_path, gt, pred, title="Val"):
     fpr, tpr, threshold = metrics.roc_curve(gt, pred)
     roc_auc = metrics.auc(fpr, tpr)
     plt.figure(figsize=(6, 6))
-    plt.title('Validation ROC')
-    plt.plot(fpr, tpr, 'b', label='Val AUC = %0.3f' % roc_auc)
+    plt.title(title+' ROC')
+    plt.plot(fpr, tpr, 'b', label=title+' AUC = %0.3f' % roc_auc)
     plt.legend(loc='lower right')
     plt.plot([0, 1], [0, 1], 'r--')
     plt.xlim([0, 1])
@@ -45,71 +47,155 @@ def draw_roc(save_path, gt, pred):
     plt.savefig(save_path)
     plt.show()
 
-i=0
-for e in range(EPOCH):
-    model1.train()
-    model2.train()
-    model3.train()
-    for id, comp in train_data.items():
-        hy, inv, tax_re, tax_pay = comp.get_train_data()
+def model_train():
+    i=0
+    for e in range(EPOCH):
+        model1.train()
+        model2.train()
+        model3.train()
+        pred1_list, pred2_list, pred3_list = [], [], []
+        gt = []
+        for id, comp in train_data.items():
+            hy, inv, tax_re, tax_pay = comp.get_train_data()
+            gt.append(comp.label)
+            re_pred, re_info = model2(tax_re)
+            pay_pred, pay_info = model3(tax_pay)
 
-        re_pred, re_info = model2(tax_re)
-        pay_pred, pay_info = model3(tax_pay)
+            pred = model1(hy, inv, re_info, pay_info)
+            pred1_list.append(F.softmax(pred, dim=-1).detach().cpu().numpy()[1])
+            pred2_list.append(F.softmax(re_pred, dim=-1).detach().cpu().numpy()[1])
+            pred3_list.append(F.softmax(pay_pred, dim=-1).detach().cpu().numpy()[1])
 
-        pred = model1(hy, inv, re_info, pay_info).unsqueeze(0)
-        label = torch.LongTensor([comp.label])
-        loss1 = criterion1(pred, label)
-        optimizer1.zero_grad()
+            label = torch.LongTensor([comp.label])
+            loss1 = criterion1(pred.unsqueeze(0), label)
+            optimizer1.zero_grad()
 
-        loss2 = criterion2(re_pred.unsqueeze(0), label)
-        optimizer2.zero_grad()
+            loss2 = criterion2(re_pred.unsqueeze(0), label)
+            optimizer2.zero_grad()
 
-        loss3 = criterion3(pay_pred.unsqueeze(0), label)
-        optimizer3.zero_grad()
-        loss1.backward(retain_graph=True)
-        loss2.backward(retain_graph=True)
-        loss3.backward()
-        optimizer2.step()
-        optimizer1.step()
-        optimizer3.step()
-        if i % 10 == 0:
-            print("com %d, loss: %.3f, %.3f, %.3f"%(i, loss1, loss2, loss3))
-        i+=1
-    # test 开始
-    acc1, acc2, acc3 = 0,0,0
-    pred1_list, pred2_list, pred3_list = [], [], []
-    gt = []
-    model1.eval()
-    model2.eval()
-    model3.eval()
-    for id, comp in eval_data.items():
-        hy, inv, tax_re, tax_pay = comp.get_train_data()
-        label = comp.label
-        gt.append(label)
-        re_pred, re_info = model2(tax_re)
-        pred2 = torch.argmax(re_pred, dim=0)
-        acc2 += (pred2 == label)
-        pay_pred, pay_info = model3(tax_pay)
-        pred3 = torch.argmax(pay_pred, dim=0)
-        acc3 += (pred3 == label)
-        pred = model1(hy, inv, re_info, pay_info)
-        pred1 = torch.argmax(pred, dim=0)
-        acc1 += (pred1==label)
-        # print(pred, pred1, label, F.softmax(pred, dim=-1).detach().cpu().numpy())
-        pred1_list.append(F.softmax(pred, dim=-1).detach().cpu().numpy()[1])
-        pred2_list.append(F.softmax(re_pred, dim=-1).detach().cpu().numpy()[1])
-        pred3_list.append(F.softmax(pay_pred, dim=-1).detach().cpu().numpy()[1])
-    draw_roc("./rocmodel1.jpg", gt, pred1_list)
-    draw_roc("./rocmodel2.jpg", gt, pred2_list)
-    draw_roc("./rocmodel3.jpg", gt, pred3_list)
-    print("in epoch %d , acc1,2,3 : %.3f, %.3f, %.3f"%(e, acc1/len(eval_data),
-                                                       acc2/len(eval_data), acc3/len(eval_data)))
-    test_res = []
-    for id, comp in test_data.items():
-        hy, inv, tax_re, tax_pay = comp.get_train_data()
-        pay_pred, _ = model3(tax_pay)
-        pop = F.softmax(pay_pred, dim=-1).detach().cpu().numpy()[1]
-        test_res.append({'SHXYDM':id, 'predict_prob':pop})
-    df = pd.DataFrame(test_res)
-    df.to_csv('test_res_ep%d.csv'%e)
+            loss3 = criterion3(pay_pred.unsqueeze(0), label)
+            optimizer3.zero_grad()
+            loss1.backward(retain_graph=True)
+            loss2.backward(retain_graph=True)
+            loss3.backward()
+            optimizer2.step()
+            optimizer1.step()
+            optimizer3.step()
+            if i % 30 == 0:
+                print("com %d, loss: %.3f, %.3f, %.3f"%(i, loss1, loss2, loss3))
+            i+=1
+        draw_roc("./vis/train_rocmodel1%d.jpg"%e, gt, pred1_list, title='train')
+        draw_roc("./vis/train_rocmodel2%d.jpg"%e, gt, pred2_list, title='train')
+        draw_roc("./vis/train_rocmodel3%d.jpg"%e, gt, pred3_list, title='train')
+        # test 开始
+        acc1, acc2, acc3 = 0,0,0
+        pred1_list, pred2_list, pred3_list = [], [], []
+        gt = []
+        model1.eval()
+        model2.eval()
+        model3.eval()
+        for id, comp in eval_data.items():
+            hy, inv, tax_re, tax_pay = comp.get_train_data()
+            label = comp.label
+            gt.append(label)
+            re_pred, re_info = model2(tax_re)
+            pred2 = torch.argmax(re_pred, dim=0)
+            acc2 += (pred2 == label)
+            pay_pred, pay_info = model3(tax_pay)
+            pred3 = torch.argmax(pay_pred, dim=0)
+            acc3 += (pred3 == label)
+            pred = model1(hy, inv, re_info, pay_info)
+            pred1 = torch.argmax(pred, dim=0)
+            acc1 += (pred1==label)
+            # print(pred, pred1, label, F.softmax(pred, dim=-1).detach().cpu().numpy())
+            pred1_list.append(F.softmax(pred, dim=-1).detach().cpu().numpy()[1])
+            pred2_list.append(F.softmax(re_pred, dim=-1).detach().cpu().numpy()[1])
+            pred3_list.append(F.softmax(pay_pred, dim=-1).detach().cpu().numpy()[1])
+        draw_roc("./vis/eval_rocmodel1%d.jpg"%e, gt, pred1_list)
+        draw_roc("./vis/eval_rocmodel2%d.jpg"%e, gt, pred2_list)
+        draw_roc("./vis/eval_rocmodel3%d.jpg"%e, gt, pred3_list)
+        print("in epoch %d , acc1,2,3 : %.3f, %.3f, %.3f"%(e, acc1/len(eval_data),
+                                                           acc2/len(eval_data), acc3/len(eval_data)))
+        if test:
+            test_res = []
+            for id, comp in test_data.items():
+                hy, inv, tax_re, tax_pay = comp.get_train_data()
+                pay_pred, _ = model3(tax_pay)
+                pop = F.softmax(pay_pred, dim=-1).detach().cpu().numpy()[1]
+                test_res.append({'SHXYDM':id, 'predict_prob':pop})
+            df = pd.DataFrame(test_res)
+            df.to_csv('test_res_ep%d.csv'%e)
 
+
+def svm_data_pre(tax_re, tax_pay):
+    m, c = tax_re.shape
+    r_m = 52
+    r_year=15
+    new_tax_re = [0]*r_m
+    j=r_year
+    new_tax_re[0:min(r_year, m)] = tax_re[0:min(r_year,m), 0]
+    for ci in range(1,c):
+        for mi in range(m):
+            if(tax_re[mi,ci]!=0):
+                new_tax_re[j] = tax_re[mi,ci]
+                j+=1
+                if j >=r_m:
+                    break
+        if j >= r_m:
+            break
+    # 处理交税
+    p_m = 7
+    new_tax_pay = [0]*p_m*3
+    j = 0
+    for it, info in tax_pay.items():
+        for info_m in info:
+            new_tax_pay[j*3:(j+1)*3] = info_m
+            j += 1
+            if j >= p_m:
+                break
+        if j >= p_m:
+            break
+    return new_tax_re, new_tax_pay
+
+def compress_data(in_data, get_label=False):
+    svm_train_x = []
+    if get_label:
+        svm_train_y = []
+    for id, comp in in_data.items():
+        hy_dm = comp.HY_DM  # 6位行业编码
+        inv = comp.get_four_investor().reshape(-1)  # 8位投资信息
+        tax_re = comp.get_tax_return()  # month * 商品种类， 选取2个商品种类，12个月的数据
+        tax_pay = comp.get_tax_pay()  # 商品种类： 各个月份， 选取2个商品种类，2个月的数据？
+        tax_re, tax_pay = svm_data_pre(tax_re, tax_pay)
+        new_info = []
+        # new_info.extend(hy_dm)
+        # new_info.extend(inv)
+        new_info.extend(tax_re)
+        new_info.extend(tax_pay)
+        svm_train_x.append(new_info)
+        if get_label:
+            label = comp.label
+            svm_train_y.append(label)
+    if get_label:
+        return np.array(svm_train_x), np.array(svm_train_y)
+    else:
+        return np.array(svm_train_x)
+
+def svm_train():
+    svm_train_x, svm_train_y = compress_data(train_data, get_label=True)
+    svm_eval_x, svm_eval_y = compress_data(eval_data, get_label=True)
+    clf = SVC(probability=True)
+    clf.fit(svm_train_x, svm_train_y)
+    pred = clf.predict( svm_eval_x )
+    eval_acc = ( np.sum( pred == svm_eval_y ) / len(svm_eval_y))
+    pred = clf.predict( svm_train_x )
+    train_acc = ( np.sum( pred == svm_train_y ) / len(svm_train_y))
+    print("eval_acc: %.3f, train acc: %.3f"%(eval_acc, train_acc))
+    predp = clf.predict_proba( svm_eval_x )
+    draw_roc("./vis/svm_eval.jpg", svm_eval_y, predp[:,1], "eval")
+    predp = clf.predict_proba(svm_train_x)
+    draw_roc("./vis/svm_train.jpg", svm_train_y, predp[:, 1], "train")
+
+
+if __name__ == '__main__':
+    svm_train()
